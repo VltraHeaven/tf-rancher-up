32c4f9446cc20bf1f75f65fec19ae3f669ae5533
Julio Hawthorne <julio@vltraheaven.io>
2024-07-12 16:40:07 -0700

    Implemented variables across doks distro module

--- modules/distribution/doks/main.tf
+++ modules/distribution/doks/main.tf
@@ -1,25 +1,31 @@
 locals {
   node_pool_count = var.node_pool_count > 1 ? "${var.node_pool_count - 1}" : 0
+  kc_path         = var.kube_config_path != null ? var.kube_config_path : path.cwd
+  kc_file         = var.kube_config_filename != null ? "${local.kc_path}/${var.kube_config_filename}" : "${local.kc_path}/${var.prefix}_kube_config.yml"
+  kc_file_backup  = "${local.kc_file}.backup"
 }
 
-resource "digitalocean_kubernetes_cluster" "auto_upgrading_cluster" {
-  name         = "${var.prefix}-${var.tag_begin}"
-  region       = data.digitalocean_regions.available_region.regions.0.slug
-  auto_upgrade = true
-  version      = data.digitalocean_kubernetes_versions.doks_versions.latest_version
-  ha           = false
+
+resource "digitalocean_kubernetes_cluster" "cluster" {
+  name                             = "${var.prefix}-${var.tag_begin}"
+  region                           = data.digitalocean_regions.available_region.regions.0.slug
+  auto_upgrade                     = var.automatic_upgrades
+  version                          = data.digitalocean_kubernetes_versions.doks_versions.latest_version
+  ha                               = var.create_ha_cluster
+  surge_upgrade                    = var.enable_surge_upgrades
+  destroy_all_associated_resources = var.destroy_digitalocean_resources
 
   maintenance_policy {
-    start_time = "04:00"
-    day        = "sunday"
+    start_time = var.automatic_upgrade_time
+    day        = var.automatic_upgrade_day
   }
 
   node_pool {
     name       = "${var.prefix}-node-pool-1"
-    size       = element(data.digitalocean_sizes.droplet_size.sizes, 0).slug 
+    size       = element(data.digitalocean_sizes.droplet_size.sizes, 0).slug
     auto_scale = true
-    min_nodes  = 1
-    max_nodes  = 5
+    min_nodes  = var.min_nodes
+    max_nodes  = var.max_nodes
     tags       = ["user:${var.user_tag}", "creator:${var.prefix}"]
     labels     = var.labels
   }
@@ -27,18 +33,26 @@ resource "digitalocean_kubernetes_cluster" "auto_upgrading_cluster" {
 
 resource "digitalocean_kubernetes_node_pool" "autoscaling-pool" {
   count      = local.node_pool_count
-  cluster_id = digitalocean_kubernetes_cluster.auto_upgrading_cluster.id
+  cluster_id = digitalocean_kubernetes_cluster.cluster.id
   name       = "${var.prefix}-node-pool-${count.index + var.tag_begin + 1}"
-  size       = element(data.digitalocean_sizes.droplet_size.sizes, 0).slug 
+  size       = element(data.digitalocean_sizes.droplet_size.sizes, 0).slug
   auto_scale = true
-  min_nodes  = 1
-  max_nodes  = 5
+  min_nodes  = var.min_nodes
+  max_nodes  = var.max_nodes
   tags       = ["user:${var.user_tag}", "creator:${var.prefix}"]
   labels     = var.labels
 }
 
-resource "local_file" "kube_config" {
-  content         = digitalocean_kubernetes_cluster.auto_upgrading_cluster.kube_config.0.raw_config
-  filename        = "./doks_kube_config.yaml"
+resource "local_file" "kube_config_yaml" {
+  count           = var.create_kubeconfig_file ? 1 : 0
+  content         = digitalocean_kubernetes_cluster.cluster.kube_config.0.raw_config
+  filename        = local.kc_file
+  file_permission = "0600"
+}
+
+resource "local_file" "kube_config_yaml_backup" {
+  count           = var.create_kubeconfig_file ? 1 : 0
+  content         = digitalocean_kubernetes_cluster.cluster.kube_config.0.raw_config
+  filename        = local.kc_file
   file_permission = "0600"
 }
--- modules/distribution/doks/variables.tf
+++ modules/distribution/doks/variables.tf
@@ -5,6 +5,13 @@ variable "do_token" {
   sensitive   = true
 }
 
+variable "kubernetes_version" {
+  type        = string
+  description = "The major and minor release version of the new cluster. A cluster may only be upgraded to newer versions in-place. If the version is decreased, a new resource will be created."
+  default     = "1.28"
+  nullable    = false
+}
+
 variable "user_tag" {
   type        = string
   description = "FirstInitialLastname of user"
@@ -43,8 +50,72 @@ variable "node_pool_droplet_size" {
   nullable    = false
 }
 
+variable "min_nodes" {
+  type        = number
+  description = "Minimum amount of nodes per node pool"
+  default     = 1
+}
+
+variable "max_nodes" {
+  type        = number
+  description = "Minimum amount of nodes per node pool"
+  default     = 5
+}
 variable "region" {
   type        = string
   description = "Target region for cluster deployment"
   default     = "sfo3"
+}
+
+variable "automatic_upgrades" {
+  type        = bool
+  description = "Enable automatic patch upgrades for the managed cluster"
+  default     = false
+}
+
+variable "automatic_upgrade_time" {
+  type        = string
+  description = "Set what time automatic upgrades will take place"
+  default     = "04:00"
+}
+
+variable "automatic_upgrade_day" {
+  type        = string
+  description = "Set which day automatic upgrades will take place"
+  default     = "sunday"
+}
+
+variable "create_ha_cluster" {
+  type        = bool
+  description = "Create a high-availablity managed cluster"
+  default     = false
+}
+
+variable "enable_surge_upgrades" {
+  type        = bool
+  description = "Reduce upgrade times by creating duplicate nodes during cluster upgrades"
+  default     = true
+}
+
+variable "create_kubeconfig_file" {
+  description = "Boolean flag to generate a kubeconfig file (mostly used for dev only)"
+  default     = true
+}
+
+variable "kube_config_path" {
+  description = "The path to write the kubeconfig for the RKE cluster"
+  type        = string
+  default     = null
+}
+
+variable "kube_config_filename" {
+  description = "Filename to write the kube config"
+  type        = string
+  default     = null
+}
+
+variable "destroy_digitalocean_resources" {
+  type = bool
+  default = true
+  description = "Destroy all associated DigitalOcean resources created via the Kubernetes API when the cluster is destroyed"
 }
