961b91ec5eadc8b35fdb72f57cabcb28deb38408
Julio Hawthorne <julio@vltraheaven.io>
2024-07-23 17:01:16 -0700

    Update to DOKS cluster outputs

--- recipes/downstream/digitalocean/doks/output.tf
+++ recipes/downstream/digitalocean/doks/output.tf
@@ -1,3 +1,7 @@
+output "cluster_name" {
+  value = module.downstream_doks.cluster_name
+}
+
 output "insecure_registration_command" {
   value = data.rancher2_cluster.doks_imported.cluster_registration_token[0].insecure_command
 }
